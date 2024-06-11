2b25c0edf18fc0382e495fd18eea595d32e7f1a0
meomeocoj <minh.nt@orai.io>
2024-06-11 14:46:07 +0700

    feat: add skeleton for relaying cosmos to ton

---
+++ packages/relayer/src/@types/global.d.ts
@@ -0,0 +1,25 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      MNEMONIC: string;
+      // SYNC_OPTS
+      COSMOS_RPC_URL: string;
+      SYNC_BLOCK_OFFSET: number;
+      SYNC_LIMIT: number;
+      SYNC_THREADS: number;
+      SYNC_INTERVAL: number;
+      // TON API
+      TON_CENTER: string;
+      TON_LITE_CLIENT_LIST: string;
+      // DB
+      CONNECTION_STRING: string;
+      // CONTRACT
+      BRIDGE_WASM_ADDRESS: string;
+      BRIDGE_TON_ADDRESS: string;
+    }
+  }
+}
+
+// If this file has no import/export statements (i.e. is a script)
+// convert it into a module by adding an empty export statement.
+export {};
--- packages/relayer/src/index.ts
+++ packages/relayer/src/index.ts
@@ -1,5 +1,61 @@
-// TODO: may transform to an express app not worker
-async function main() {}
+import { SyncDataOptions, Txs } from "@oraichain/cosmos-rpc-sync";
+import { envConfig } from "./config";
+import { DuckDb } from "./duckdb.service";
+import { CosmosBlockOffset } from "./models/cosmwasm/block-offset";
+import {
+  CosmwasmWatcherEvent,
+  createCosmosBridgeWatcher,
+} from "./cosmos.service";
+import { BridgeParsedData } from "./@types/interfaces/cosmwasm";
+
+// TODO: may transform to an express app not only worker
+async function main() {
+  const duckDb = await DuckDb.getInstance(envConfig.CONNECTION_STRING);
+  const blockOffset = new CosmosBlockOffset(duckDb);
+  await blockOffset.createTable();
+  const offset = await blockOffset.mayLoadBlockOffset(
+    envConfig.SYNC_BLOCK_OFFSET
+  );
+
+  const syncDataOpt: SyncDataOptions = {
+    rpcUrl: envConfig.COSMOS_RPC_URL,
+    limit: envConfig.SYNC_LIMIT,
+    maxThreadLevel: envConfig.SYNC_THREADS,
+    offset: offset,
+    interval: envConfig.SYNC_INTERVAL,
+    queryTags: [],
+  };
+
+  if (offset < envConfig.SYNC_BLOCK_OFFSET) {
+    syncDataOpt.offset = envConfig.SYNC_BLOCK_OFFSET;
+  }
+
+  if (envConfig.BRIDGE_WASM_ADDRESS === "") {
+    throw new Error("BRIDGE_WASM_ADDRESS is required");
+  }
+
+  const cosmosWatcher = createCosmosBridgeWatcher(
+    envConfig.BRIDGE_WASM_ADDRESS,
+    syncDataOpt
+  );
+
+  // UPDATE BLOCK OFFSET TO DATABASE
+  const database = await DuckDb.getInstance(envConfig.CONNECTION_STRING);
+  const blockOffSet = new CosmosBlockOffset(database);
+  cosmosWatcher.on(CosmwasmWatcherEvent.SYNC_DATA, async (chunk: Txs) => {
+    const { offset: newOffset } = chunk;
+    await blockOffSet.updateBlockOffset(newOffset);
+    console.log("Update new offset at", newOffset);
+  });
+
+  // LISTEN ON THE PARSED_DATA FROM WATCHER
+  cosmosWatcher.on(
+    CosmwasmWatcherEvent.PARSED_DATA,
+    async (data: BridgeParsedData) => {
+      const { submitData, submittedTxs } = data;
+    }
+  );
+}
 
 main().catch((error) => {
   console.log(error);
