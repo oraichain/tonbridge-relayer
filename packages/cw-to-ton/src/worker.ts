83be86a10b688501648df54dcbbb1e3ef5e340fc
meomeocoj <minh.nt@orai.io>
2024-08-14 17:20:01 +0700

    feat: wrap retry to waitSeqno

--- packages/cw-to-ton/src/worker.ts
+++ packages/cw-to-ton/src/worker.ts
@@ -66,7 +66,17 @@ export const createTonWorker = (
             3,
             5000
           );
-          await waitSeqno(walletContract, await walletContract.getSeqno(), 15);
+          retry(
+            async () => {
+              await waitSeqno(
+                walletContract,
+                await walletContract.getSeqno(),
+                15
+              );
+            },
+            3,
+            1000
+          );
           await sleep(30000); // TODO: Alter by tracing transaction to get the result
         } catch (error) {
           throw new Error(`[TON-WORKER] Update light client failed: ${error}`);
@@ -92,7 +102,13 @@ export const createTonWorker = (
         5000
       );
       console.log("[TON-WORKER] Relay packet successfully");
-      await waitSeqno(walletContract, await walletContract.getSeqno(), 15);
+      retry(
+        async () => {
+          await waitSeqno(walletContract, await walletContract.getSeqno(), 15);
+        },
+        3,
+        1000
+      );
     },
     {
       connection,
