89415ca9c1d5d3803d90c499c448cf7381a797ed
meomeocoj <minh.nt@orai.io>
2026-01-15 14:23:33 +0700

    docs: update README.md

--- packages/cw-to-ton/src/scripts/relayAckTimeout.ts
+++ packages/cw-to-ton/src/scripts/relayAckTimeout.ts
@@ -4,60 +4,62 @@ import { Tendermint37Client } from "@cosmjs/tendermint-rpc";
 import { parseWasmEvents } from "@oraichain/oraidex-common";
 import {
   BridgeAdapter,
+  getAckPacketProofs,
   getExistenceProofSnakeCell,
   getPacketProofs,
 } from "@oraichain/ton-bridge-contracts";
 import { Network } from "@orbs-network/ton-access";
-import { TransferPacket } from "../dtos/packets/TransferPacket";
-import { BRIDGE_WASM_ACTION } from "../services";
-import { createTonWallet, waitSeqno } from "../utils";
+import { AckPacket } from "@src/dtos/packets/AckPacket";
+import { TransferPacket } from "@src/dtos/packets/TransferPacket";
+import { BRIDGE_WASM_ACTION } from "@src/services";
+import { createTonWallet, waitSeqno } from "@src/utils";
 import { Address, toNano } from "@ton/core";
 import { ExistenceProof } from "cosmjs-types/cosmos/ics23/v1/proofs";
 import * as dotenv from "dotenv";
 dotenv.config();
 
 (async () => {
-  const provenHeight = 72368119;
+  const provenHeight = 57123222;
   const packetTx =
-    "9BD5B746C421AA57224D9A3B0688F50BCE2757C7D91B7595652E2007AD2CE7B6";
+    "AECCC0074B6091AB61946E6211CF61609CFC7375326252EEBBF5C0D2E3972ECE";
   const needProvenHeight = provenHeight + 1;
   const { client, walletContract, key } = await createTonWallet(
-    process.env.TON_MNEMONIC!,
+    process.env.TON_MNEMONIC,
     process.env.NODE_ENV as Network
   );
+
   const bridgeAdapter = BridgeAdapter.createFromAddress(
-    Address.parse(process.env.TON_BRIDGE!)
+    Address.parse(process.env.TON_BRIDGE)
   );
+
   const bridgeAdapterContract = client.open(bridgeAdapter);
-  console.log(await bridgeAdapterContract.getBridgeData());
-  // process.env.COSMOS_RPC_URL!
+  console.log("Connected to TON");
+
   const cosmwasmClient = await CosmWasmClient.connect(
-    "http://3.14.142.99:26657"
+    process.env.COSMOS_RPC_URL
   );
-  console.log(packetTx);
-  console.log(walletContract.address);
-  console.log(await walletContract.getBalance());
-  // return;
+  console.log("Connected to Cosmos", process.env.COSMOS_RPC_URL);
+
   const tx = await cosmwasmClient.getTx(packetTx);
-  console.log(tx);
-  const wasmAttr = parseWasmEvents(tx!.events);
+  const wasmAttr = parseWasmEvents(tx.events);
+  console.log(wasmAttr);
   const filterByContractAddress = (attr: Record<string, string>) =>
     attr["_contract_address"] === process.env.WASM_BRIDGE;
   // This action come from user need to normalize and submit by relayer.
   const sendToTonEvents = wasmAttr
     .filter(filterByContractAddress)
-    .filter((attr) => attr["action"] === BRIDGE_WASM_ACTION.SEND_TO_TON);
+    .filter((attr) => attr["action"] === BRIDGE_WASM_ACTION.SEND_TO_COSMOS);
   const packetEvent = sendToTonEvents[0];
 
-  const transferPacket = TransferPacket.fromRawAttributes(packetEvent);
+  const transferPacket = AckPacket.fromRawAttributes(packetEvent);
   const tendermint37 = await Tendermint37Client.connect(
-    process.env.COSMOS_RPC_URL as string
+    process.env.COSMOS_RPC_URL
   );
   const queryClient = new QueryClient(tendermint37 as any);
   console.log(provenHeight, BigInt(packetEvent["seq"]));
-  const packetProofs = await getPacketProofs(
+  const packetProofs = await getAckPacketProofs(
     queryClient,
-    process.env.WASM_BRIDGE as string,
+    process.env.WASM_BRIDGE,
     provenHeight,
     BigInt(packetEvent["seq"])
   );
@@ -65,13 +67,12 @@ dotenv.config();
   const proofs = packetProofs.map((proof) => {
     return ExistenceProof.fromJSON(proof);
   });
-
   await bridgeAdapterContract.sendBridgeRecvPacket(
     walletContract.sender(key.secretKey),
     {
       provenHeight: needProvenHeight,
       packet: transferPacket.intoCell(),
-      proofs: getExistenceProofSnakeCell(proofs as any)!,
+      proofs: getExistenceProofSnakeCell(proofs as any),
     },
     { value: toNano("0.7") }
   );
